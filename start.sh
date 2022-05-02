98e36cb0290016fd0daf2de35495e53e3dbd0a5e
mercymurigi27 <mercy.murigi@student.moringaschool.com>
2022-05-02 16:16:58 +0300

    change secret key

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export API_KEY = '9e870d9b9bf94c8489704e8e59e8f7ef'
-export SECRET_KEY = 'kittenarecute'
+export API_KEY='9e870d9b9bf94c8489704e8e59e8f7ef'
+export SECRET_KEY='kittenarecute'
 
 python3 manage.py server

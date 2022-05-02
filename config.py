58ef86f623d05d40e09d0530ffc415cfde79bc80
mercymurigi27 <mercy.murigi@student.moringaschool.com>
2022-05-02 16:17:32 +0300

    add bootstrap

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
 
+
 class Config:
     '''
     General configuration parent class
@@ -9,3 +10,29 @@ class Config:
     ARTICLE_BASE_URL = "https://newsapi.org/v2/top-headlines?sources={}&apiKey={}"
     API_KEY = os.environ.get('API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+    
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig
+    }
